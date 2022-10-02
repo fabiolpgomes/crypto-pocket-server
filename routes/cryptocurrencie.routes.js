98fe5855e2d01e5ef36bb6f3ad9e82d6a61ce230
Bruno Apostolo <bruno.apos13@gmail.com>
2022-10-02 18:39:42 -0300

    duas rotas adicionadas

---
+++ routes/cryptocurrencie.routes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const UserModel = require("../models/User.model");
+const WalletModel = require("../models/Wallet.model");
+const CryptocurrencieModel = require("../models/CryptoCurrencie.model");
+const isAuth = require("../middlewares/isAuth");
+const attachCurrentUser = require("../middlewares/attachCurrentUser");
+const router = express.Router();
+
+router.post("/purchasecoin/:idWallet", async (req, res) => {
+  try {
+    const { idWallet } = req.params;
+    const criatingCoin = await CryptocurrencieModel.create({
+      ...req.body,
+      nome: req.body.nome,
+      aporte: req.body.aporte,
+      wallet: idWallet,
+    });
+    const attachingToWallet = await WalletModel.findByIdAndUpdate(
+      idWallet,
+      {
+        $push: { crypto: criatingCoin._id },
+      },
+      { new: true }
+    );
+    return res.status(200).json(attachingToWallet);
+  } catch (error) {
+    return res.status(400).json("CREDO");
+  }
+});
+module.exports = router;
---
+++ routes/wallets.routes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const UserModel = require("../models/User.model");
+const WalletModel = require("../models/Wallet.model");
+const CryptocurrencieModel = require("../models/CryptoCurrencie.model");
+const isAuth = require("../middlewares/isAuth");
+const attachCurrentUser = require("../middlewares/attachCurrentUser");
+const router = express.Router();
+
+router.post("/createwallet", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const loggedUser = req.currentUser;
+    if (loggedUser.wallets.length > 0 && loggedUser.signatureType === "BASIC") {
+      return res.status(200).json({
+        mensagem:
+          "Você não pode adicionar mais nenhuma carteira com esse plano",
+      });
+    }
+    const newWallet = await WalletModel.create({
+      ...req.body,
+      owner: loggedUser._id,
+    });
+    const walletAttachment = await UserModel.findByIdAndUpdate(
+      loggedUser._id,
+      { $push: { wallets: newWallet._id } },
+      { new: true }
+    );
+
+    return res.status(201).json(walletAttachment);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json("ERRO");
+  }
+});
+
+module.exports = router;
